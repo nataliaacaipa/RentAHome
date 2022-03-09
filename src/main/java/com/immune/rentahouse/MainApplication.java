ee105b84b99c3ca150fbe17ed0ee93a250186612
Natalia <lisnatalia2002.lnmc@gmail.com>
2022-03-09 13:00:05 +0100

    refactor

--- src/main/java/com/immune/rentahouse/MainApplication.java
+++ src/main/java/com/immune/rentahouse/MainApplication.java
@@ -1,4 +1,4 @@
-package com.immune.tienda;
+package com.immune.rentahouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/immune/rentahouse/controller/LoginController.java
+++ src/main/java/com/immune/rentahouse/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.immune.tienda.controller;
+package com.immune.rentahouse.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
