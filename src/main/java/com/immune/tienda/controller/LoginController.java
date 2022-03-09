3fb7b2994410f2c70fd7b46c9af1c2f7e237a9e9
Natalia <lisnatalia2002.lnmc@gmail.com>
2022-03-09 12:46:19 +0100

    delete cosas

---
+++ src/main/java/com/immune/tienda/controller/LoginController.java
@@ -0,0 +1,27 @@
+package com.immune.tienda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+    
+    @GetMapping("/login")
+    public ModelAndView login() {
+
+        ModelAndView model = new ModelAndView("index");
+        try {
+            System.out.println(model.getViewName());
+            return model;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ModelAndView("error");
+        }
+
+
+    }
+
+}
